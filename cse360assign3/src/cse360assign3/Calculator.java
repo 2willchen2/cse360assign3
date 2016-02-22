818661be74432b1dc5fca33e91226399b111375d
Will <wchen122@asu.edu>
2016-02-22 03:18:38 -0700

    Version 3 Calculator

--- cse360assign3/src/cse360assign3/Calculator.java
+++ cse360assign3/src/cse360assign3/Calculator.java
@@ -1,11 +1,11 @@
 
-
 package cse360assign3;
 
 public class Calculator {
 
 	private int total;
-	
+	private String history = "";
+	private int count = 0;
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
@@ -15,16 +15,45 @@ public class Calculator {
 	}
 	
 	public void add (int value) {
+		//adds on the total
 		total += value;
-		
+		if(count == 0)
+		{
+			//check if this is the first operation calculated
+			history += Integer.toString(value);
+		}
+		else
+		{
+			//if not then add the symbol and operation
+			history += " + " + value;
+		}
+		count++;
 	}
 	
 	public void subtract (int value) {
 		total -= value;
+		if(count == 0)
+		{
+			history += Integer.toString(value);
+		}
+		else
+		{
+			history += " - " + value;
+		}
+		count++;
 	}
 	
 	public void multiply (int value) {
 		total = total * value;
+		if(count == 0)
+		{
+			history += Integer.toString(value);
+		}
+		else
+		{
+			history += " * " + value;
+		}
+		count++;
 	}
 	
 	public void divide (int value) {
@@ -34,11 +63,21 @@ public class Calculator {
 		}
 		else
 		{
-		total = total/value;
+			total = total/value;
 		}
+		if(count == 0)
+		{
+			history += Integer.toString(value);
+		}
+		else
+		{
+			history += " / " + value;
+		}
+		count++;
 	}
 	
 	public String getHistory () {
-		return "";
+		//just returns the string of operation
+		return history;
 	}
 }
